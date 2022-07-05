6d8419a4b3bd915752665cd25b51899a37bc1169
anteCedens <jaksa.juric@yahoo.com.hr>
2022-07-05 17:54:24 +0000

    Set index (default) route&view

--- run.py
+++ run.py
@@ -5,6 +5,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP', '0.0.0.0'),
